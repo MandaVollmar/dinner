6f0c01727fc03315cd2ca893a51f4cb9ab9aeb0e
Amanda Vollmar <mandavollmar@Amandas-MBP.hsd1.co.comcast.net>
2022-02-16 09:39:43 -0700

    Added Arrays of Dishes

--- src/com/company/Dinner.java
+++ src/com/company/Dinner.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class Dinner {
 
     public static void main(String[] args) {
 	// write your code here
+        Italian myDish = new Italian();
+        Mexican myDish2 = new Mexican();
+
+
+        String [] italianFood = {"Bolognese", "Fettuccini", "Spaghetti", "Lasagna", "Chicken Parmesan" };
+        String [] mexicanFood = {"Tacos", "Nachos", "Chili Reanos", "Enchiladas", "Fajitas"};
+
+
+
     }
 }
